0b064b1543f3b5bcf306887292534da1e70aebc3
GaryRam <126gramirez@frhsd.com>
2025-03-03 14:02:15 -0500

    doof

--- js/js.js
+++ js/js.js
@@ -1,4 +1,5 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake.js";
+import { update as updateFood, draw as drawFood} from './food.js';
 
 let lastRenderTime = 0;
 const gameBoard = document.getElementById
@@ -8,11 +9,12 @@ const gameBoard = document.getElementById
 function main(currentTime) {
     window.requestAnimationFrame(main);
     const secondSinceLastRender = (currentTime - lastRenderTime) / 1000;
+
     if (secondSinceLastRender < 1 / SNAKE_SPEED) return;
 
 
-    lastRenderTime = currentTime;
     console.log('Render');
+    lastRenderTime = currentTime;
 
     update();
     draw();
@@ -22,9 +24,11 @@ window.requestAnimationFrame(main);
 
 function update(){
 updateSnake();
+updateFood();
 }
 
 function draw(){
     gameBoard.innerHTML = '';
 drawSnake(gameBoard);
+drawFood(gameBoard);
 }
--- js/snake.js
+++ js/snake.js
@@ -1,19 +1,19 @@
-export const SNAKE_SPEED = 2;
-const snakeBody = [
-    { x: 10, y: 11},
-    { x: 11, y: 11},
-    { x: 12, y: 11},
-    { x: 13, y: 11},
-    { x: 14, y: 11},
-    { x: 15, y: 11}
-]
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 5;
+const snakeBody = [{ x: 11, y: 11}];
+let newSegments = 0;
+
 export function update(){
+    const inputDirection = getInputDirection();
+
     for(let i = snakeBody.length - 2; i >= 0; i--) {
         snakeBody[i + 1] = { ...snakeBody[i] };
     }
 
-    snakeBody[0].x += 0;
-    snakeBody[0].y += 1;
+    snakeBody[0].x += inputDirection.x;
+   snakeBody[0].y += inputDirection.y;
+
 }
 
 export function draw (gameBoard){
@@ -26,3 +26,16 @@ export function draw (gameBoard){
     })
 }
 
+export function expandSnake(amount) {
+    newSegments += amount;
+}
+
+export function onSnake(position) {
+    return snakeBody.some(segment => {
+        return equalPosition(segment, position);
+    })
+}
+
+function equalPosition(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y
+}
